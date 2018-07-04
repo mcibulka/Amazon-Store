f7de8497421381136453942a58492530b9da5203
Matthew Cibulka <mcibulkadev@gmail.com>
2018-07-03 22:35:38 -0400

    Added user input to purchase an item and quantity, db is updated accordingly.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var cTable = require("console.table");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host     : "localhost",
@@ -21,5 +22,54 @@ connection.query("SELECT item_id, product_name, price FROM products", function (
     console.table(results);
     console.log("\n");
 });
+
+
+function getUserInput() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_id",
+            message: "Enter the Item ID of the product you would like to buy:"
+        },
+        {
+            type: "input",
+            name: "units",
+            message: "How many units of the item?"
+        }
+    ]).then(answers => {
+        var availStockQuery = `SELECT item_id, price, stock_quantity FROM products
+                               WHERE item_id = "${answers.item_id}";`;
+
+                               
+        connection.query(availStockQuery, function (error, results, fields) {
+            var unitPrice = results[0].price;
+
+            if (error) throw error;
+
+            if (answers.units <= results[0].stock_quantity) {
+                var newQuantity = results[0].stock_quantity - answers.units;
+
+                var purchaseQuery = `UPDATE products
+                                     SET stock_quantity = "${newQuantity}"
+                                     WHERE item_id = "${answers.item_id}";`
+
+
+                connection.query(purchaseQuery, function (error, results, fields) {
+                    if (error) throw error;
+
+                    var totalCost = unitPrice * answers.units;
+                    
+                    console.log(`\nTransaction SUCCESSFUL: You owe \$${totalCost}\n`);
+                });
+            }
+            else {
+                console.log("\nTransaction FAILED: Insufficient stock available.\n");
+            }
+        });
+    });
+}
+
+setTimeout(getUserInput, 500);
+
  
-connection.end();
+// connection.end();
