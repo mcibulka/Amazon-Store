58361752c41fa19076419a3e712783a85a02c982
Matthew Cibulka <mcibulkadev@gmail.com>
2018-07-08 12:37:34 -0400

    Added view all products and low inventory.

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,6 +15,34 @@ console.log(`\n\t\t\tWelcome to BAMazon`);
 console.log(`\t\t\t==================\n`);
 
 function getUserInput() {
+    
+    function listItems() {
+        var query = `SELECT item_id, product_name, price, stock_quantity FROM products`;
+
+        connection.query(query, function (error, results, fields) {
+            if (error) throw error;
+
+            console.log("\n");
+            console.table(results);
+            console.log("\n");
+        });
+    }
+
+
+    function lowInventory() {
+        var query = `SELECT item_id, product_name, stock_quantity FROM products
+                     WHERE stock_quantity < 100`;
+
+        connection.query(query, function (error, results, fields) {
+            if (error) throw error;
+
+            console.log("\n");
+            console.table(results);
+            console.log("\n");
+        }); 
+    }
+
+
     inquirer.prompt([
         {
             type: "list",
@@ -25,10 +53,10 @@ function getUserInput() {
     ]).then(answers => {
         switch (answers.menuChoice) {
             case "View Products for Sale":
-                console.log("View Products");
+                listItems();
                 break;
             case "View Low Inventory":
-                console.log("Low Inventory");
+                lowInventory();
                 break;
             case "Add to Inventory":
                 console.log("Add Inventory");
